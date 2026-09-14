2e96ef6480adbaa26bbb1bbaf68c9e8c90c1f7f5
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2026-09-13 21:53:00 -0700

    t-1 renaming layout-components

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,5 @@
 export { cx } from "./cx";
-export { Page, Header, Main, Row, Nav, Stack } from "./layout";
+export { Page, Header, Main, Row, Nav, Stack } from "./layout-components";
 export { Heading, Text, Muted, Empty } from "./text";
 export { Button, CheckButton } from "./button";
 export { Field, Input, Textarea, Hidden } from "./field";
